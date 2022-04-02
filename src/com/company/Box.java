730dac8da298af4acfe32bc05eb9969634b0a341
BazzBHeat <andreysaykonsobol@gmail.com>
2022-04-02 23:19:55 +0300

    Homework11 -> Homework12

--- src/com/company/Box.java
+++ src/com/company/Box.java
@@ -2,17 +2,88 @@ package com.company;
 
 public class Box {
 
-    double length;
-    double width;
-    double height;
+    private double length;
+    private double width;
+    private double height;
+    private String colour;
+    private String material;
 
-    Box(double length, double width, double height){
+  public   Box(double length, double width, double height){
         this.length = length;
         this.width = width;
         this.height = height;
     }
 
-    double volume() {
+   public Box(double length, double width, double height, String colour, String material){
+        this(length, width, height);
+        this.colour = colour;
+        this.material = material;
+
+    }
+
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setLength(double length) {
+        if(length > 0) {
+            this.length = length;
+        } else if(length == 0) {
+            this.length = 1;
+        } else {
+                this.length = Math.abs(length);
+            }
+        }
+
+
+    public void setWidth(double width) {
+        if(width > 0) {
+            this.width = width;
+        } else if (width == 0){
+            this.width = 1;
+        }
+        else {
+            this.width = Math.abs(width);
+        }
+    }
+
+    public void setHeight(double height) {
+        if(height > 0) {
+            this.height = height;
+        } else if (height == 0) {
+            this.height = 1;
+        }
+            else {
+            this.height = Math.abs(height);
+        }
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public double volume() {
         return (length * width * height);
     }
 }
--- src/com/company/Homework11.java
+++ src/com/company/Homework11.java
@@ -4,14 +4,17 @@ public class Homework11 {
 
     public static void main(String[] args) {
 	Box box1 = new Box(10,10,10);
+    box1.setLength(-1);
     Box box2 = new Box(20,30,40);
     Box box3 = new Box(30,40,50);
     Box box4 = new Box(40,50,60);
     Box box5 = new Box(50,60,70);
+    Box box6 = new Box(1,1,1,"Black","Steel");
         System.out.println("Объём коробки 1 = " + box1.volume());
         System.out.println("Объём коробки 2 = " + box2.volume());
         System.out.println("Объём коробки 3 = " + box3.volume());
         System.out.println("Объём коробки 4 = " + box4.volume());
         System.out.println("Объём коробки 5 = " + box5.volume());
+        System.out.println("This box is " + box6.getColour() + " and made of " + box6.getMaterial());
     }
 }
